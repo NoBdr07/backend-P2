18c1782045a38c961a85e9dfe697a1919e4e9b35
Noémie <baudry.ddf@gmail.com>
2024-06-09 21:49:23 +0200

    ajout de commentaires

--- src/main/java/com/bdr/backend/configuration/SpringSecurityConfig.java
+++ src/main/java/com/bdr/backend/configuration/SpringSecurityConfig.java
@@ -33,12 +33,12 @@ public class SpringSecurityConfig {
 	public CorsFilter corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true); // Allow credentials (e.g., cookies) to be sent in the CORS request.
-		config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Allow requests from the specified origin.
-		config.addAllowedHeader("*"); // Allow all headers to be sent in the request.
-		config.addAllowedMethod("*");// Allow all HTTP methods (e.g., GET, POST, etc.).
-		source.registerCorsConfiguration("/**", config); // Apply the CORS configuration to all paths.
-		return new CorsFilter(source); // Create and return the CorsFilter.
+		config.setAllowCredentials(true); 
+		config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); 
+		config.addAllowedHeader("*"); 
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source); 
 	}
 
 	// List of routes that do not require authentication
@@ -54,7 +54,6 @@ public class SpringSecurityConfig {
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(
 						auth -> auth
-						.requestMatchers(org.springframework.http.HttpMethod.OPTIONS).permitAll() 
 						.requestMatchers(AUTH_WHITELIST).permitAll()
 						.anyRequest().authenticated())
 				.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())).build();
